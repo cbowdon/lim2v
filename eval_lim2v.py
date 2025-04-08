190e6c02fe15cdb85ecd2d2721f3155de463c211
Chris Bowdon <cbowdon@example.com>
2025-04-08 21:58:22 +0800

    Fix distance issue: normalise
    Except... this destroys important information in Potion. The vectors are scaled by frequency estimate, which is destroyed by normalisation.

--- eval_lim2v.py
+++ eval_lim2v.py
@@ -2,17 +2,20 @@ import numpy as np
 from collections import defaultdict
 from faiss import IndexFlatIP
 from model2vec import StaticModel
+from polars import DataFrame
 from eval_sbert import load_passages, load_qrels, load_queries
 
 potion = StaticModel.from_pretrained("minishlab/potion-base-8M", normalize=True)
 
+l2_norm = np.linalg.norm(potion.embedding, axis=1, keepdims=True) + 1e-32
+norm_embedding = potion.embedding / l2_norm
+
+faiss_index = IndexFlatIP(potion.dim)
+faiss_index.add(norm_embedding)
+
 limit = 1000
 
 pids, passages = load_passages(limit=limit)
-
-faiss_index = IndexFlatIP(potion.dim)
-faiss_index.train(potion.embedding)
-faiss_index.add(potion.embedding)
 
 doc_index = defaultdict(set)
 tids = potion.tokenize(passages)
@@ -28,14 +31,20 @@ for pid, _tids in zip(pids, tids):
 # make a map of document token to max query token similarity
 # and then for each document with some of these tokens we sum the max sims
 
-query = potion.encode_as_sequence("how many units of blood in a human body")
-D, I = faiss_index.search(query, k=10)
+query = "how many units of blood in a human body"
+qembeds = potion.encode_as_sequence(query)
+qembeds /= np.linalg.norm(qembeds, axis=1, keepdims=True) + 1e-32
 
-# TODO why are the distances not in [0, 1]?
+D, I = faiss_index.search(qembeds, k=10)
 
 result_map = defaultdict(int)
 for qt, closest_to_query_token in enumerate(I):
     for rank, tid in enumerate(closest_to_query_token):
         docs = doc_index[tid]
         for doc in docs:
-            result_map[doc] += 1 - D[qt, rank].item()
+            result_map[doc] += D[qt, rank].item()
+
+d2 = DataFrame(list(result_map.items()), schema=["doc_id", "score"], orient="row").sort(
+    "score", descending=True
+)
+d2.head()
