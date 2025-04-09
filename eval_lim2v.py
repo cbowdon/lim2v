23d4fb178a5001e68b50e909a9c99c2c6b0b58a1
Chris Bowdon <cbowdon@example.com>
2025-04-09 22:14:12 +0800

    Reading the colbert paper again

--- eval_lim2v.py
+++ eval_lim2v.py
@@ -3,6 +3,8 @@ from collections import defaultdict
 from faiss import IndexFlatIP, IndexFlatL2
 from model2vec import StaticModel
 from polars import DataFrame
+from scipy.sparse import lil_matrix, dok_matrix
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.neighbors import NearestNeighbors
 from eval_sbert import load_passages, load_qrels, load_queries
 
@@ -25,36 +27,55 @@ limit = 1000
 pids, passages = load_passages(limit=limit)
 df_p = DataFrame({"pid": pids, "passage": passages})
 
-doc_index = defaultdict(set)
-tids = potion.tokenize(passages)
-for pid, _tids in zip(pids, tids):
-    for tid in _tids:
-        doc_index[tid].add(pid)
+# Exhaustively:
+# 3 dimensional tensor of k document embedding matrices, D
+# compute batch dot product of Eq and D
+# max pool docs
+# sum over Eq
+#
+# Pruning way:
+# Maintain mapping of embedding to doc
+# Faiss to get the unique docs
+# Then go to exhaustive approach
 
-# for each query and document,
-# for each query token q:
-# what is the most sim document token in each document
-# - and sum across query tokens
-# if we get the top k vocab tokens for each q
-# then we work down the ranks, assigning each doc its max score
-# and sum across q
+doc_toks = lil_matrix((len(passages), len(potion.tokens)), dtype=np.int32)
+for i, toks in enumerate(potion.tokenize(passages)):
+    doc_toks[i, toks] = 1
+doc_toks = doc_toks.tocsc()  # more efficient for wide sparse matrices
+
+# human body blood
+[1535, 1309, 1674]
+
+# get the docs in columns matching the search tokens with np.nonzero
+# and then get the tokens in those docs (likewise)
+
+
+all_toks = np.ravel(I)
+doc_idxs, _  = np.nonzero(doc_toks[:,all_toks])
+
+results = []
+for doc in all_docs:
+    _, tok_idxs = np.nonzero(doc_toks[doc,:])
+    doc_score = 0
+    for qt in query_tokens:
+        dists = ...
+        doc_score += min(dists)
+    results.append({ "pid": doc, "score": doc_score })
+    break
+
+query = "how many units of blood in a human body"
 
 # Turns out in ColBERT they use negative squared L2 distance
 # for the similarity metric, with norm'd vectors.
-# Unfortunately this doesn't solve the problem anyway:
-# the potion token weights are still not captured in distance so
-# we lose the knowledge of what's a meaningful word
-
-query = "how many units of blood in a human body"
 qembeds = potion.encode_as_sequence(query)
 qembed_norm = np.linalg.norm(qembeds, axis=1, keepdims=True) + 1e-32
 
 D, I = faiss_index.search(qembeds / qembed_norm, k=10)
-D = -np.pow(D, 2)
+D = -(D**2)
 # D, I = nn.kneighbors(qembeds, n_neighbors=10, return_distance=True)
 # D = 1 - D  # convert to sim
 
-result_map = {}
+
 for qt, ranked_vocab_tokens in enumerate(I):
     qt_map = {}
     for rank, tid in enumerate(ranked_vocab_tokens):
