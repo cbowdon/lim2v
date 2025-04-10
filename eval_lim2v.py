5575e19c3c93809228fb00031474ff522fc3e406
Chris Bowdon <cbowdon@example.com>
2025-04-10 19:42:03 +0800

    Implement ColBERT end-to-end search
    Note that it obviously suffers with the lack of contextualisation in two ways:
    
    - initial rough search is quite useless
    - we do see dud hits in max sim

--- eval_lim2v.py
+++ eval_lim2v.py
@@ -1,28 +1,20 @@
 import numpy as np
-from collections import defaultdict
-from faiss import IndexFlatIP, IndexFlatL2
+from faiss import IndexFlatL2
 from model2vec import StaticModel
+from numpy.typing import NDArray
 from polars import DataFrame
-from scipy.sparse import lil_matrix, dok_matrix
-from sklearn.metrics.pairwise import euclidean_distances
-from sklearn.neighbors import NearestNeighbors
+from scipy.sparse import lil_matrix
 from eval_sbert import load_passages, load_qrels, load_queries
 
 potion = StaticModel.from_pretrained("minishlab/potion-base-8M", normalize=True)
 
 l2_norm = np.linalg.norm(potion.embedding, axis=1, keepdims=True) + 1e-32
-norm_embedding = potion.embedding / l2_norm
-
-# faiss_index = IndexFlatIP(potion.dim)
-# faiss_index.add(norm_embedding)
+norm_tok_embeds = potion.embedding / l2_norm
 
 faiss_index = IndexFlatL2(potion.dim)
-faiss_index.add(norm_embedding)
+faiss_index.add(norm_tok_embeds)
 
-nn = NearestNeighbors(metric="cosine")
-nn.fit(potion.embedding)
-
-limit = 1000
+limit = 10_000
 
 pids, passages = load_passages(limit=limit)
 df_p = DataFrame({"pid": pids, "passage": passages})
@@ -38,61 +30,70 @@ df_p = DataFrame({"pid": pids, "passage": passages})
 # Faiss to get the unique docs
 # Then go to exhaustive approach
 
-doc_toks = lil_matrix((len(passages), len(potion.tokens)), dtype=np.int32)
+doc_tok_mat = lil_matrix((len(passages), len(potion.tokens)), dtype=np.int32)
 for i, toks in enumerate(potion.tokenize(passages)):
-    doc_toks[i, toks] = 1
-doc_toks = doc_toks.tocsc()  # more efficient for wide sparse matrices
-
-# human body blood
-[1535, 1309, 1674]
-
-# get the docs in columns matching the search tokens with np.nonzero
-# and then get the tokens in those docs (likewise)
+    doc_tok_mat[i, toks] = 1
+doc_tok_mat = doc_tok_mat.tocsc()  # more efficient for wide sparse matrices
 
 
-all_toks = np.ravel(I)
-doc_idxs, _  = np.nonzero(doc_toks[:,all_toks])
-
-results = []
-for doc in all_docs:
-    _, tok_idxs = np.nonzero(doc_toks[doc,:])
-    doc_score = 0
-    for qt in query_tokens:
-        dists = ...
-        doc_score += min(dists)
-    results.append({ "pid": doc, "score": doc_score })
-    break
-
-query = "how many units of blood in a human body"
-
-# Turns out in ColBERT they use negative squared L2 distance
-# for the similarity metric, with norm'd vectors.
-qembeds = potion.encode_as_sequence(query)
-qembed_norm = np.linalg.norm(qembeds, axis=1, keepdims=True) + 1e-32
-
-D, I = faiss_index.search(qembeds / qembed_norm, k=10)
-D = -(D**2)
-# D, I = nn.kneighbors(qembeds, n_neighbors=10, return_distance=True)
-# D = 1 - D  # convert to sim
+def embed(query: str) -> NDArray[np.float32]:
+    qembeds = potion.encode_as_sequence(query)
+    qembed_norm = np.linalg.norm(qembeds, axis=1, keepdims=True) + 1e-32
+    return qembeds / qembed_norm
 
 
-for qt, ranked_vocab_tokens in enumerate(I):
-    qt_map = {}
-    for rank, tid in enumerate(ranked_vocab_tokens):
-        docs = doc_index[tid]
-        for doc in docs:
-            if doc not in qt_map:
-                qt_map[doc] = D[qt, rank].item()
-    for doc, score in qt_map.items():
-        if doc not in result_map:
-            result_map[doc] = score
-        else:
-            result_map[doc] += score
+def rough_search(Eq: NDArray[np.float32], k: int = 5) -> NDArray[np.int32]:
+    """With non-contextualised embeddings, this is less useful because of high overlap - needs weighting/pruning."""
+    D, I = faiss_index.search(Eq, k)
+    all_toks = np.unique(np.ravel(I))
+    doc_idxs, _ = np.nonzero(doc_tok_mat[:, all_toks])
+    result = np.unique(doc_idxs)
+    return np.sort(result)
 
 
-d2 = (
-    DataFrame(list(result_map.items()), schema=["pid", "score"], orient="row")
-    .join(df_p, on="pid")
-    .sort("score", descending=True)
-)
-d2.head()
+def pad_embeds(E: NDArray[np.float32], target: int) -> NDArray[np.float32]:
+    n_to_pad = target - E.shape[0]
+    if n_to_pad <= 0:
+        return E
+    pad_emb = norm_tok_embeds[0]
+    pads = np.vstack([pad_emb for _ in range(n_to_pad)])
+    return np.vstack((E, pads))
+
+
+def get_doc_tok_mat(
+    doc_ids: NDArray[np.int32], *, minlen: int = 0
+) -> NDArray[np.float32]:
+    _D = []
+    maxlen = minlen
+    for doc in doc_tok_mat[doc_ids]:
+        _, toks = np.nonzero(doc)
+        maxlen = max(maxlen, len(toks))
+        _D.append(norm_tok_embeds[toks])
+
+    pad_emb = norm_tok_embeds[0]
+    D = [pad_embeds(_d, maxlen) for _d in _D]
+
+    return np.array(D)
+
+
+def exhaustive_search(Eq: NDArray[np.float32], doc_ids: NDArray[np.int32]):
+    D = get_doc_tok_mat(doc_ids, minlen=Eq.shape[0])
+    D_flat = D.reshape(-1, D.shape[2])
+    S_flat = np.dot(D_flat, Eq.T)
+    S = S_flat.reshape(D.shape[0], D.shape[1], Eq.shape[0])
+    max_sims = S.max(axis=1)  # n_docs x n_query_toks
+    scores = max_sims.sum(axis=1)  # n_docs
+    sorting = scores.argsort()[::-1]
+    return doc_ids[sorting], np.sort(scores)[::-1]
+
+
+Eq = embed("how many units of blood in the human body")
+
+doc_ids = rough_search(Eq)
+doc_ids = np.arange(len(passages), dtype=np.int32)
+
+results = exhaustive_search(Eq, doc_ids)
+
+df_p[results[0]]
+
+# human body blood: [1535, 1309, 1674]
